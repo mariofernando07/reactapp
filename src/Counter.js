46af6d533529e0bdd688eb7118b0fb12c3c5a564
Mario Riascos <marioriascos@mriascoss.local>
2024-09-19 12:22:26 -0500

    increase the count value by click event

--- src/Counter.js
+++ src/Counter.js
@@ -1,35 +1,42 @@
 import { useState, useEffect } from 'react';
 import './Counter.css'
 
-function Counter({init, end}) {
-    init = parseInt(init || 0)
-    end = parseInt(end || 0)
-    const [count, setCount] = useState(init)
+function Counter({ init, end }) {
+  init = parseInt(init || 0)
+  end = parseInt(end || 0)
+  const [count, setCount] = useState(init)
 
-    useEffect(() => {
-        if (end && count >= end) return;
-        var timer = setInterval(() => {
-            // setCount(count+1);
-            setCount((count) => count + 1)
-            console.log(`Count = ${count}`)
-        }, 1000)
+  // useEffect(() => {
+  //   if (end && count >= end) return;
+  //   var timer = setInterval(() => {
+  //     // setCount(count+1);
+  //     setCount((count) => count + 1)
+  //     console.log(`Count = ${count}`)
+  //   }, 1000)
 
-        return () => {clearInterval(timer)}
-    })
+  //   return () => { clearInterval(timer) }
+  // })
 
-    return (
-      <div className='counter-container'>
-        Initial value of the counter is: {init}
-        <br />
-        End of the counter at: {end}
-        <br />
-        The counter is set to: {count}
-        <br />
-        {
-            (end && count >= end) ? <b style={{color: "red", fontSize: "20px"}}>Counter stopped</b> : <i>Counter in progress</i>
-        }
-      </div>
-    );
+  function incr() {
+    setCount((count) => count + 1);
   }
-  
-  export default Counter;
+
+  return (
+    <div className='counter-container'>
+      Initial value of the counter is: {init}
+      <br />
+      End of the counter at: {end}
+      <br />
+      The counter is set to: {count}
+      <br />
+      {
+        (end && count >= end) ? <b style={{ color: "red", fontSize: "20px" }}>Counter stopped</b> : <>
+          <i>Counter in progress</i>
+          <button onClick={incr}>count+1</button>
+        </>
+      }
+    </div>
+  );
+}
+
+export default Counter;
