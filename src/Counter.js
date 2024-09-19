33489d42d54f1a8ebdba6ed5a2a31dcffbed0c45
Mario Riascos <marioriascos@mriascoss.local>
2024-09-19 12:36:33 -0500

    add method to start increasing the counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,40 +1,51 @@
-import { useState, useEffect } from 'react';
-import './Counter.css'
+import { useState, useEffect } from "react";
+import "./Counter.css";
 
 function Counter({ init, end }) {
-  init = parseInt(init || 0)
-  end = parseInt(end || 0)
-  const [count, setCount] = useState(init)
+  init = parseInt(init || 0);
+  end = parseInt(end || 0);
+  const [count, setCount] = useState(init);
+  const [start, setStart] = useState(true); // true for displaying the Start button
 
-  // useEffect(() => {
-  //   if (end && count >= end) return;
-  //   var timer = setInterval(() => {
-  //     // setCount(count+1);
-  //     setCount((count) => count + 1)
-  //     console.log(`Count = ${count}`)
-  //   }, 1000)
+  useEffect(() => {
+    if (!start) {
+      var timer = setInterval(() => {
+        setCount((count) => {
+          var newCount = count + 1;
+          if (newCount >= end) setStart(true);
+          return newCount;
+        });
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  });
 
-  //   return () => { clearInterval(timer) }
-  // })
-
-  function incr() {
-    setCount((count) => count + 1);
+  function restart() {
+    setStart(false); // Hide the Start button
+    setCount(init); // Reset "count" to the initial value
   }
 
   return (
-    <div className='counter-container'>
+    <div className="counter-container">
       Initial value of the counter is: {init}
       <br />
       End of the counter at: {end}
       <br />
       The counter is set to: {count}
       <br />
-      {
-        (end && count >= end) ? <b style={{ color: "red", fontSize: "20px" }}>Counter stopped</b> : <>
-          <i>Counter in progress</i>
-          <button onClick={incr}>count+1</button>
+      {start ? (
+        <>
+          <b style={{ color: "red", fontSize: "20px" }}>Counter stopped</b>
+          &nbsp;
+          <button onClick={restart}>Start</button>
         </>
-      }
+      ) : (
+        <>
+          <i>Counter in progress</i>
+        </>
+      )}
     </div>
   );
 }
